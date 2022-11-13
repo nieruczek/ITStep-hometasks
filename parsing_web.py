8010855585a20d1d0658da85b106c4c6cf20857b
nieruczek <116871955+nieruczek@users.noreply.github.com>
2022-11-13 18:32:20 +0100

    Add files via upload

---
+++ parsing_web.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os.path
+
+path = os.path.join("", "text1.txt")
+
+
+agent_007 = webdriver.Chrome(executable_path="D:\\GALYA\\PythonProjects\\pythonProject\\chromedriver.exe")
+
+agent_007.get(url="https://www.ukr.net/")
+
+#a = agent_007.title
+
+wiki = agent_007.find_elements(By.CLASS_NAME, "feed__item--title")
+my_file = open(path, "w")
+
+for i in wiki:
+    my_file.write(i.text + "\n")
+
+agent_007.close()
+
+my_file.close()
